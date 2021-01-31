500a5b122a265b80d8d5ce66f1e5166eb3ade8b5
Xu Jing <jingxu086@gmail.com>
2021-01-31 01:54:37 -0500

    feat(update): update game

--- cs.js
+++ cs.js
@@ -1,32 +1,192 @@
 var colorSudoku = (function () {
     var COLORS = [
         "#E74C3C", "#8E44AD", "#5DADE2",
-        "#1ABC9C", "#52BE80", "#F7DC6F",
-        "#F39C12", "#5D6D7E", "#D2B4DE"
+        "#1ABC9C", "#4dd888", "#F7DC6F",
+        "#F39C12", "#226dbd", "#D2B4DE"
     ]
-    var init, init_board, init_game_gui, init_game,
+    var BOARDS = {
+        3: [
+            "123 231 312",
+            "123 312 231",
+        ],
+        4: [
+            "1234 2341 3412 4123",
+            "1234 2341 4123 3412",
+            "1234 3412 2341 4123",
+            "1234 3412 4123 2341",
+            "1234 4123 2341 3412",
+            "1234 4123 3412 2341",
+        ],
+        5: [
+            "12345 23451 34512 45123 51234",
+            "12345 23451 34512 51234 45123",
+            "12345 23451 45123 34512 51234",
+            "12345 23451 45123 51234 34512",
+            "12345 23451 51234 34512 45123",
+            "12345 23451 51234 45123 34512",
+            "12345 34512 23451 45123 51234",
+            "12345 34512 23451 51234 45123",
+            "12345 34512 45123 23451 51234",
+            "12345 34512 45123 51234 23451",
+            "12345 34512 51234 23451 45123",
+            "12345 34512 51234 45123 23451",
+            "12345 45123 23451 34512 51234",
+            "12345 45123 23451 51234 34512",
+            "12345 45123 34512 23451 51234",
+            "12345 45123 34512 51234 23451",
+            "12345 45123 51234 23451 34512",
+            "12345 45123 51234 34512 23451",
+            "12345 51234 23451 34512 45123",
+            "12345 51234 23451 45123 34512",
+            "12345 51234 34512 23451 45123",
+            "12345 51234 34512 45123 23451",
+            "12345 51234 45123 23451 34512",
+            "12345 51234 45123 34512 23451",
+        ],
+        6: [
+            "123456 234561 345612 456123 561234 612345",
+            "123456 234561 345612 456123 612345 561234",
+            "123456 234561 345612 561234 456123 612345",
+            "123456 234561 345612 561234 612345 456123",
+            "123456 234561 345612 612345 456123 561234",
+            "123456 234561 345612 612345 561234 456123",
+            "123456 234561 456123 345612 561234 612345",
+            "123456 234561 456123 345612 612345 561234",
+            "123456 234561 456123 561234 345612 612345",
+            "123456 234561 456123 561234 612345 345612",
+            "123456 234561 456123 612345 345612 561234",
+            "123456 234561 456123 612345 561234 345612",
+            "123456 234561 561234 345612 456123 612345",
+            "123456 234561 561234 345612 612345 456123",
+            "123456 234561 561234 456123 345612 612345",
+            "123456 234561 561234 456123 612345 345612",
+            "123456 234561 561234 612345 345612 456123",
+            "123456 234561 561234 612345 456123 345612",
+            "123456 234561 612345 345612 456123 561234",
+            "123456 234561 612345 345612 561234 456123",
+            "123456 234561 612345 456123 345612 561234",
+            "123456 234561 612345 456123 561234 345612",
+            "123456 234561 612345 561234 345612 456123",
+            "123456 234561 612345 561234 456123 345612",
+            "123456 345612 234561 456123 561234 612345",
+            "123456 345612 234561 456123 612345 561234",
+            "123456 345612 234561 561234 456123 612345",
+            "123456 345612 234561 561234 612345 456123",
+            "123456 345612 234561 612345 456123 561234",
+            "123456 345612 234561 612345 561234 456123",
+            "123456 345612 456123 234561 561234 612345",
+            "123456 345612 456123 234561 612345 561234",
+            "123456 345612 456123 561234 234561 612345",
+            "123456 345612 456123 561234 612345 234561",
+            "123456 345612 456123 612345 234561 561234",
+            "123456 345612 456123 612345 561234 234561",
+            "123456 345612 561234 234561 456123 612345",
+            "123456 345612 561234 234561 612345 456123",
+            "123456 345612 561234 456123 234561 612345",
+            "123456 345612 561234 456123 612345 234561",
+            "123456 345612 561234 612345 234561 456123",
+            "123456 345612 561234 612345 456123 234561",
+            "123456 345612 612345 234561 456123 561234",
+            "123456 345612 612345 234561 561234 456123",
+            "123456 345612 612345 456123 234561 561234",
+            "123456 345612 612345 456123 561234 234561",
+            "123456 345612 612345 561234 234561 456123",
+            "123456 345612 612345 561234 456123 234561",
+            "123456 456123 234561 345612 561234 612345",
+            "123456 456123 234561 345612 612345 561234",
+            "123456 456123 234561 561234 345612 612345",
+            "123456 456123 234561 561234 612345 345612",
+            "123456 456123 234561 612345 345612 561234",
+            "123456 456123 234561 612345 561234 345612",
+            "123456 456123 345612 234561 561234 612345",
+            "123456 456123 345612 234561 612345 561234",
+            "123456 456123 345612 561234 234561 612345",
+            "123456 456123 345612 561234 612345 234561",
+            "123456 456123 345612 612345 234561 561234",
+            "123456 456123 345612 612345 561234 234561",
+            "123456 456123 561234 234561 345612 612345",
+            "123456 456123 561234 234561 612345 345612",
+            "123456 456123 561234 345612 234561 612345",
+            "123456 456123 561234 345612 612345 234561",
+            "123456 456123 561234 612345 234561 345612",
+            "123456 456123 561234 612345 345612 234561",
+            "123456 456123 612345 234561 345612 561234",
+            "123456 456123 612345 234561 561234 345612",
+            "123456 456123 612345 345612 234561 561234",
+            "123456 456123 612345 345612 561234 234561",
+            "123456 456123 612345 561234 234561 345612",
+            "123456 456123 612345 561234 345612 234561",
+            "123456 561234 234561 345612 456123 612345",
+            "123456 561234 234561 345612 612345 456123",
+            "123456 561234 234561 456123 345612 612345",
+            "123456 561234 234561 456123 612345 345612",
+            "123456 561234 234561 612345 345612 456123",
+            "123456 561234 234561 612345 456123 345612",
+            "123456 561234 345612 234561 456123 612345",
+            "123456 561234 345612 234561 612345 456123",
+            "123456 561234 345612 456123 234561 612345",
+            "123456 561234 345612 456123 612345 234561",
+            "123456 561234 345612 612345 234561 456123",
+            "123456 561234 345612 612345 456123 234561",
+            "123456 561234 456123 234561 345612 612345",
+            "123456 561234 456123 234561 612345 345612",
+            "123456 561234 456123 345612 234561 612345",
+            "123456 561234 456123 345612 612345 234561",
+            "123456 561234 456123 612345 234561 345612",
+            "123456 561234 456123 612345 345612 234561",
+            "123456 561234 612345 234561 345612 456123",
+            "123456 561234 612345 234561 456123 345612",
+            "123456 561234 612345 345612 234561 456123",
+            "123456 561234 612345 345612 456123 234561",
+            "123456 561234 612345 456123 234561 345612",
+            "123456 561234 612345 456123 345612 234561",
+            "123456 612345 234561 345612 456123 561234",
+            "123456 612345 234561 345612 561234 456123",
+            "123456 612345 234561 456123 345612 561234",
+            "123456 612345 234561 456123 561234 345612",
+            "123456 612345 234561 561234 345612 456123",
+            "123456 612345 234561 561234 456123 345612",
+            "123456 612345 345612 234561 456123 561234",
+            "123456 612345 345612 234561 561234 456123",
+            "123456 612345 345612 456123 234561 561234",
+            "123456 612345 345612 456123 561234 234561",
+            "123456 612345 345612 561234 234561 456123",
+            "123456 612345 345612 561234 456123 234561",
+            "123456 612345 456123 234561 345612 561234",
+            "123456 612345 456123 234561 561234 345612",
+            "123456 612345 456123 345612 234561 561234",
+            "123456 612345 456123 345612 561234 234561",
+            "123456 612345 456123 561234 234561 345612",
+            "123456 612345 456123 561234 345612 234561",
+            "123456 612345 561234 234561 345612 456123",
+            "123456 612345 561234 234561 456123 345612",
+            "123456 612345 561234 345612 234561 456123",
+            "123456 612345 561234 345612 456123 234561",
+            "123456 612345 561234 456123 234561 345612",
+            "123456 612345 561234 456123 345612 234561",
+        ]
+    }
+
+    var init_board, init_game_gui, init_game, init_board_gui,
         get_board,
-        array_copy, array_exclude, array_nd, array_choose_n, get_block_xy,
-        sample, check, get_user_inp, check_inp, has_dup, random_level,
+        array_nd, array_choose, array_choose_n, get_block_xy,
+        check, get_user_inp, check_inp, has_dup, random_level, shuffle, rand_int,
         config = {
             board_size: 3,
             colors: null,
-            level: 5,
-            enable_block: false
+            level: 3,
+            enable_block: false,
+            game_end: false
         },
         stat = {
             color: null
         };
 
-    init = function () {
-        init_game();
-    }
-
     init_game = function () {
-        config.board_size = array_choose_n([3, 4], 1);
         config.level = random_level(config.board_size);
         config.colors = array_choose_n(COLORS, config.board_size);
-        config.enable_block = config.board_size >= 9 && Math.sqrt(config.board_size) % 1 === 0;
+        config.enable_block = config.board_size === 9;
+        config.game_end = false;
         var game_board = get_board(config.board_size, config.enable_block);
         game_board = init_board(game_board, config.level);
         init_board_gui(config.board_size, game_board, config.colors);
@@ -34,14 +194,17 @@ var colorSudoku = (function () {
     }
 
     init_board = function (board, num_hole) {
-        var size = board.length;
-        while (num_hole > 0) {
-            var i = Math.floor(Math.random() * size),
-                j = Math.floor(Math.random() * size);
-            if (board[i][j] > 0) {
-                board[i][j] = 0;
-                num_hole--;
+        var lst = [];
+        for (var i = 0; i < config.board_size; i++) {
+            for (var j = 0; j < config.board_size; j++) {
+                lst.push([i, j]);
             }
+        }
+        var data = array_choose_n(lst, num_hole);
+        for (var idx = 0; idx < num_hole; idx++) {
+            var i = data[idx][0];
+            var j = data[idx][1];
+            board[i][j] = 0;
         }
         return board;
     }
@@ -71,7 +234,7 @@ var colorSudoku = (function () {
 
     random_level = function (size) {
         var size = size * size,
-            min = Math.ceil(size * 0.3),
+            min = Math.ceil(size * 0.4),
             max = Math.floor(size * 0.7),
             rst = Math.floor(Math.random() * (max - min + 1) + min);
         return rst;
@@ -89,8 +252,28 @@ var colorSudoku = (function () {
         return array;
     }
 
+    rand_int = function(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    array_choose = function (array) {
+        return array[rand_int(array.length)];
+    }
+
     array_choose_n = function (array, n) {
-        return array.sort(function () {return Math.random() - Math.random();}).slice(0, n)
+        return shuffle(array).slice(0, n)
+    }
+
+    shuffle = function (array) {
+        var last = array.length;
+        while (last > 0) {
+            --last;
+            var n = rand_int(last);
+            var q = array[n];
+            array[n] = array[last];
+            array[last] = q;
+        }
+        return array;
     }
 
     check_inp = function () {
@@ -140,24 +323,10 @@ var colorSudoku = (function () {
             msg_box = document.getElementById("msg-box");
         if (rst) {
             msg_box.innerHTML = "Congrats!";
+            config.game_end = true;
         } else {
             msg_box.innerHTML = "";
         }
-    }
-
-    array_exclude = function (a, b) {
-        return a.filter(function(x) {return b.indexOf(x) < 0 })
-    }
-
-    array_copy = function (array) {
-        return JSON.parse(JSON.stringify(array));
-    }
-
-    sample = function (array) {
-        if (!array || array.length === 0) return;
-        var idx = Math.floor(Math.random() * array.length);
-        var rst = array[idx];
-        return rst;
     }
 
     get_block_xy = function (i, j, block_size) {
@@ -167,59 +336,21 @@ var colorSudoku = (function () {
     }
 
     get_board = function (size, use_block) {
-        var pass = false,
-            block_size = Math.floor(Math.sqrt(size));
-
-        var array_temp = [], block_temp = [], nums = [];
-            for (var idx=0; idx<size; idx++) {
-                nums.push(idx+1);
+        var data = array_choose(BOARDS[size]).split(" ");
+        var board = [];
+        for (var i = 0; i < data.length; i++) {
+            var row_str = data[i];
+            var sub_lst = [];
+            for (var j = 0; j < row_str.length; j++) {
+                sub_lst.push(parseInt(row_str[j]));
             }
-            array_temp = array_nd([size, size]);
-            if (use_block) {
-                var b_size = block_size * block_size;
-                block_temp = array_nd([b_size, b_size])
-            }
-        while (!pass) {
-            var cols = array_copy(array_temp),
-                rows = array_copy(array_temp),
-                blocks = array_copy(block_temp);
-
-            for (var i=0; i<size; i++) {
-                var skip_loop = false;
-                for (var j=0; j<size; j++) {
-                    // var block_idx = (Math.floor(i / block_size) * block_size) + Math.floor(j / block_size);
-                    var block_xy = get_block_xy(i, j, block_size);
-                    var available_items = array_exclude(nums, cols[i]);
-                    available_items = array_exclude(available_items, rows[j]);
-                    if (use_block) {
-                        available_items = array_exclude(available_items, blocks[block_xy[0]]);
-                    }
-                    var num = sample(available_items);
-                    if (!num) {
-                        skip_loop = true;
-                        break;
-                    }
-                    cols[i][j] = num;
-                    rows[j][i] = num;
-                    if (use_block) {
-                        blocks[block_xy[0]][block_xy[1]] = num;
-                    }
-                    if (i === size - 1 && j === size - 1) {
-                        pass = true;
-                    }
-                }
-                if (skip_loop) {
-                    break;
-                }
-            }
-            if (pass) {
-                return cols;
-            }
+            board.push(sub_lst);
         }
+        return board;
     }
 
     init_board_gui = function (size, game_board, colors) {
-        var boardW = 400;
+        var boardW = document.getElementsByClassName("game-board")[0].offsetWidth;
         var board = document.createElement("table");
         board.id = "board";
         board.style.width = boardW + "px";
@@ -229,14 +360,22 @@ var colorSudoku = (function () {
 
         var boxSize = Math.floor(boardW / size) - 4;
         var box = document.createElement("td");
-        box.classList.add("board-box");
+        box.classList.add("board-box", "ear");
         box.style.width = boxSize + "px";
         box.style.height = boxSize + "px";
         var boardTrTemp = document.createElement("tr");
 
+        if (size === 5) {
+            box.classList.add("board-box-sm");
+        } else if (size === 6) {
+            box.classList.add("board-box-xs");
+        }
+
         var box_on_click = function () {
             var box = this,
                 color = stat.color;
+            if (config.game_end) return;
+
             if (box.getAttribute("data-val") === color) {
                 box.style.backgroundColor = "";
                 box.setAttribute("data-val", "");
@@ -257,6 +396,11 @@ var colorSudoku = (function () {
                     ele.classList.add("board-box-fixed");
                     ele.style.backgroundColor = color;
                     ele.setAttribute("data-val", color);
+
+                    var icon = document.createElement("div");
+                    icon.classList.add("board-box-fixed-icon");
+                    ele.appendChild(icon);
+
                 } else {
                     ele.setAttribute("data-val", "");
                     ele.addEventListener("click", box_on_click);
@@ -271,12 +415,13 @@ var colorSudoku = (function () {
     }
 
     init_game_gui = function (colors) {
-        var box = document.getElementById("btn-color-box");
-        // var btn_w = Math.floor(box.clientWidth * 0.8 / colors.length);
+        var box1 = document.getElementById("btn-color-box-1");
+        var box2 = document.getElementById("btn-color-box-2");
         var btn_color_temp = document.createElement("div");
         btn_color_temp.classList.add("btn-color");
         var class_select = "btn-color-select";
-        box.innerHTML = "";
+        box1.innerHTML = "";
+        box2.innerHTML = "";
         document.getElementById("msg-box").innerHTML = "";
 
         var btn_color_on_click = function () {
@@ -300,16 +445,32 @@ var colorSudoku = (function () {
             }
             ele.setAttribute("data-color", colors[idx]);
             ele.addEventListener("click", btn_color_on_click);
-            box.append(ele);
+
+            if ((config.board_size === 5 && idx > 2) || (config.board_size === 6 && idx > 3)) {
+                box2.append(ele);
+            } else {
+                box1.append(ele);
+            }
         }
 
         document.getElementById("btn-new").addEventListener("click", function () {
             init_game();
         })
+
+        var level_btns = document.getElementsByClassName("level");
+        var on_click_level_btn = function () {
+            document.querySelector(".level.level-selected").classList.remove("level-selected");
+            this.classList.add("level-selected");
+            config.board_size = parseInt(this.getAttribute("data-size"));
+            init_game();
+        }
+        for (var idx = 0; idx < level_btns.length; idx++) {
+            level_btns[idx].addEventListener("click", on_click_level_btn);
+        }
     }
 
     return {
-        init: init
+        init: init_game
     }
 })();
 
